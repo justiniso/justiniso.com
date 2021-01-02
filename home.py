1d72317b717b02cd11ddb61ee234ffe30f8218b7
Justin Iso <justin.james.iso@gmail.com>
2021-01-02 10:54:23 -0500

    update to remove links

--- home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('home', __name__, template_folder='frontend/templates')
-
-
-@bp.route('/')
-def home():
-    return render_template('home.html')
